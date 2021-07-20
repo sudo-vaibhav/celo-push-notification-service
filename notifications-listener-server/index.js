7be481f8fc84cd975ad01376e7112551fa54346f
sudo-vaibhav <mailvaibhavchopra@gmail.com>
2021-07-20 18:57:38 +0530

    started working on notifications listener

---
+++ notifications-listener-server/index.js
@@ -0,0 +1,25 @@
+const PORT = process.env.PORT || 8000;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+app.use(express.json());
+const { MONGO_URL } = require("./secrets");
+const morgan = require("morgan");
+app.use(morgan("dev"));
+mongoose
+  .connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("connected to DB");
+
+    app.post("/notification-subscription", (req, res) => {
+      res.status(201).send("ok");
+    });
+
+    app.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+    });
+  });
