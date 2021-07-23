653d52f1e085a4307f15c57f88ce80a097dd84d8
sudo-vaibhav <mailvaibhavchopra@gmail.com>
2021-07-23 10:03:37 +0530

    added file picker

--- channel-manager-app/src/views/Dashboard/AddOrEditChannel/index.js
+++ channel-manager-app/src/views/Dashboard/AddOrEditChannel/index.js
@@ -7,31 +7,43 @@ const AddOrEditChannel = () => {
       <div className="container my-4">
         <Formik
           initialValues={{
-            title: "",
+            name: "",
+            description: "",
+            iconHash: "",
+            badgeHash: "",
           }}
+          validate={(values) => {}}
         >
-          <Form className="grid gap-8">
-            <FormField
-              name="name"
-              placeholder="Ubeswap"
-              description="The name of your channel that will be shown to the users. Generally this should be the name of your service or company, something which your users are familiar with."
-            />
-            <FormField
-              name="description"
-              description="A brief about what value the channel aims to provide in preferably less than 125 characters."
-            />
-            <FormField
-              name="icon"
-              type="file"
-              description="A square image (min 512x512 pixels). This icon will show on notifications. Your Company logo should go here."
-            />
-            <FormField
-              name="badge"
-              type="file"
-              description="A square image (min 96x96 pixels). This badge will show when notification is minimized. For example: on Android phones, this badge will appear on notifications bar if notifications panel is not open."
-            />
-            <button className="btn btn-primary">Save</button>
-          </Form>
+          {({ values, setValues }) => {
+            return (
+              <Form className="grid gap-8">
+                <FormField
+                  name="name"
+                  placeholder="Ubeswap"
+                  description="The name of your channel that will be shown to the users. Generally this should be the name of your service or company, something which your users are familiar with."
+                />
+                <FormField
+                  name="description"
+                  description="A brief about what value the channel aims to provide in preferably less than 125 characters."
+                />
+                <FormField
+                  name="iconHash"
+                  type="file"
+                  values={values}
+                  setValues={setValues}
+                  description="A square image (min 512x512 pixels). This icon will show on notifications. Your Company logo should go here."
+                />
+                <FormField
+                  name="badgeHash"
+                  type="file"
+                  values={values}
+                  setValues={setValues}
+                  description="A square image (min 96x96 pixels). This badge will show when notification is minimized. For example: on Android phones, this badge will appear on notifications bar if notifications panel is not open."
+                />
+                <button className="btn btn-primary">Save</button>
+              </Form>
+            );
+          }}
         </Formik>
       </div>
     </div>
