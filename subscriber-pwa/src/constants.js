67eb8ce7c8bbae76f605c9cc0a2499bdb3e9c8a2
sudo-vaibhav <mailvaibhavchopra@gmail.com>
2021-07-25 12:25:26 +0530

    temporary url change

--- subscriber-pwa/src/constants.js
+++ subscriber-pwa/src/constants.js
@@ -9,6 +9,6 @@ export const APPLICATION_SERVER_PUBLIC_KEY =
   "BKKJxWcSwvkMsv6RBUMeCZecpJT6x1cRTrf-TjUUumY1pdj3-tTTwLXpexBkgUU1vfv8mHLOXakStA7-yYSVS0s";
 export const subscriptionUrl = window.location.href.includes(":300")
   ? "http://192.168.29.8:8000/notification-subscription"
-  : "https://b8298825305a.ngrok.io/notification-subscription";
+  : "http://c85b31e0ade3.ngrok.io/notification-subscription";
 
 export const ALLOWED_NETWORK = Alfajores;
