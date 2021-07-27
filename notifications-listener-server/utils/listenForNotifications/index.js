d182fe400e0edf43bc694692d648a7a2f103308f
sudo-vaibhav <mailvaibhavchopra@gmail.com>
2021-07-27 19:54:26 +0530

    removed comments

--- notifications-listener-server/utils/listenForNotifications/index.js
+++ notifications-listener-server/utils/listenForNotifications/index.js
@@ -1,40 +1,7 @@
-// const { contract } = require("../contractKit");
 const sendNotifications = require("../sendNotifications");
 const Web3 = require("web3");
 const PushNotifications = require("../../contract/PushNotifications.json");
 const { ALLOWED_NETWORK } = require("../../constants");
-// const cUSD_abi = require("cUSD_abi.json");
-
-// const listenForNotifications = () => {
-//   console.log("running notifications listener");
-//   contract.events.allEvents(
-//     {
-//       fromBlock: "latest", // continue listening from now, practically a subscription to all events
-//     },
-//     async (error, event) => {
-//       console.log("event detected: ", event);
-//       if (error) {
-//         console.log("some error occured", error);
-//       } else {
-//         try {
-//           if (event.event) {
-//             console.log("I'm in and i was: ", event);
-//             await sendNotifications(event, event.event);
-//           }
-//         } catch (e) {
-//           console.log(
-//             "error in sending notifications after detecting event: ",
-//             e
-//           );
-//         }
-//       }
-//     }
-//   );
-// };
-
-// let watchedAddresses = ["0x765DE816845861e75A25fCA122bb6898B8B1282a"]; // cUSD contract
-// let topics = [];
-// let lastSeenBlock = null;
 
 function setupProviderAndSubscriptions() {
   console.log("setting up websocket connection");
@@ -104,33 +71,8 @@ function setupProviderAndSubscriptions() {
       }
     }
   );
-
-  // let headerSubscription = web3.eth.subscribe("newBlockHeaders");
-
-  // headerSubscription.on("data", function (blockHeader) {
-  //   if (sequentialRetryCount > 0) {
-  //     sequentialRetryCount = 0;
-  //   }
-  //   lastSeenBlock = blockHeader.number;
-  // });
-
-  // let eventSubscription = web3.eth.subscribe("logs", {
-  //   address: watchedAddresses,
-  //   fromBlock: lastSeenBlock,
-  //   topics: topics,
-  // });
-
-  // eventSubscription.on("data", function (data) {
-  //   console.log(data);
-  // });
-
-  // eventSubscription.on("error", async (error) => {
-  //   console.log("Block header subscription encountered an error", error);
-  //   await setupNewProviderAndSubs();
-  // });
 }
 
-// setupProviderAndSubscriptions();
 function sleep(ms, onSleep) {
   return new Promise((resolve) => {
     setTimeout(resolve, ms);
